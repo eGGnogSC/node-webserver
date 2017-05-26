148e8bb2aca202202e5952b33d616d76884585a5
Kyle Greenwell <Kyle Greenwell>
2017-05-26 08:16:56 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+//Middleware
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs', {
+//     maintenanceMessage: 'You have reached a page that is still under construction.'
+//   });
+// });
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+  var now = new Date().toISOString();
+  var log = (`${now} ${req.method} ${req.url}`);
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log.');
+    }
+  });
+  next();
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+//Routes
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my express homepage using Handlebars!'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Bad Request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
